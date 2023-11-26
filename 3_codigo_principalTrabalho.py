515a32a26d56fc4bd05bb38d786c67b9bf8b323a
Marcela Almeida <112910605+gamatecnologies@users.noreply.github.com>
2023-11-26 08:50:26 -0300

    Update 3_codigo_principalTrabalho.py

--- 3_codigo_principalTrabalho.py
+++ 3_codigo_principalTrabalho.py
@@ -4,10 +4,9 @@
 # CODIGO PRINCIPAL
 # Descrição: Problema da loja de jóias Gammasi, o objetivo é saber qual a quantidade correta
 # de materiais precisa ser usado para gerar o lucro maximo.
-
 def print_table(table):
     for row in table:
-        print(" ".join(map(lambda x: f"{x:.2f}", row)))
+        print(" ".join(map(str, row)))
 
 def simplex_maximize(c, A, b):
     m, n = len(A), len(c)
@@ -15,31 +14,33 @@ def simplex_maximize(c, A, b):
     # Multiplica por -1 para transformar o problema de minimização em maximização
     c = [-x for x in c]
 
-    # Adicionando variáveis de folga e criando a tabela inicial
-    table = [[0.0] * (n + m + 1) for _ in range(m + 1)]
+    # Adicionando variáveis de folga e criando a tabela inicial com números inteiros
+    table = [[0] * (n + m + 1) for _ in range(m + 1)]
     for i in range(m):
         table[i + 1][:n] = A[i]
-        table[i + 1][n + i] = 1.0
+        table[i + 1][n + i] = 1
         table[i + 1][-1] = b[i]
 
     table[0][:n] = c
 
-    print("Tabela Inicial:")
-    print_table(table)
-
     iteration = 1
     while any(x < 0 for x in table[0][:-1]):
+        # Encontrar a coluna do elemento pivô (mais negativo)
         pivot_col = min(range(n + m), key=lambda i: table[0][i])
 
+        # Calcular as razões para encontrar a linha do elemento pivô
         ratios = [(i, table[i][-1] / table[i][pivot_col]) for i in range(1, m + 1) if table[i][pivot_col] > 0]
         if not ratios:
             break
 
+        # Encontrar a linha do elemento pivô com a menor razão
         pivot_row = min(ratios, key=lambda x: x[1])[0]
 
+        # Normalizar a linha do elemento pivô
         pivot_value = table[pivot_row][pivot_col]
-        table[pivot_row] = [x / pivot_value for x in table[pivot_row]]
+        table[pivot_row] = [x // pivot_value for x in table[pivot_row]]
 
+        # Atualizar as outras linhas da tabela
         for i in range(m + 1):
             if i != pivot_row:
                 factor = table[i][pivot_col]
@@ -47,23 +48,21 @@ def simplex_maximize(c, A, b):
 
         print(f"\nIteração {iteration}:")
         print_table(table)
-        iteration += 3
+        iteration += 1
 
     # Extrai as variáveis originais
-    x_optimal = [table[i][-1] if i < m + 1 else 0.0 for i in range(8)]
-    Z_optimal = table[0][-1]  
+    x_optimal = [table[i][-1] if i < m + 1 else 0 for i in range(n)]
+    Z_optimal = table[0][-1]  # Mantendo o sinal negativo
 
     print("\nSolução Ótima encontrada:")
     print_table(table)
-    print(f"Z* (Quantidade de material necessário) = {Z_optimal:.2f}")
-    print(f"Ouro = {x_optimal[1]:.2f}")
-    print(f"Prata = {x_optimal[2]:.2f}")
-    print(f"Metal = {x_optimal[3]:.2f}")
-    print(f"Reciclável = {x_optimal[4]:.2f}")
-    print(f"Acrílico = {x_optimal[5]:.2f}")
-    print(f"Nylon = {x_optimal[6]:.2f}")
-    print(f"Gancho = {x_optimal[7]:.2f}")
-    
+    print(f"Z* (Valor máximo): {Z_optimal}")
+    print(f"Número de colares: {x_optimal[0]}")
+    print(f"Número de pulseiras: {x_optimal[1]}")
+    print(f"Número de brincos: {x_optimal[2]}")
+    print(f"Número de anéis: {x_optimal[3]}")
+
+    return x_optimal, Z_optimal
 
 # Coeficientes da função objetivo para maximização
 c_maximize = [50, 30, 20, 10]
@@ -74,7 +73,7 @@ A_maximize = [
     [26, 16, 10, 30],
     [20, 10, 5, 25],
     [5, 3, 2, 0],
-    [3, 2, 0, 0],
+    [0, 2, 0, 0],  # Acrílico
     [0, 4, 8, 4],
     [8, 4, 0, 0]
 ]
@@ -83,5 +82,8 @@ A_maximize = [
 b_maximize = [300, 600, 100, 25, 10, 50, 25]
 
 # Resolvendo o problema de programação linear para maximização
-simplex_maximize(c_maximize, A_maximize, b_maximize)
+solution = simplex_maximize(c_maximize, A_maximize, b_maximize)
+
+# A solução ótima pode ser acessada da seguinte forma:
+x_optimal, Z_optimal = solution
 
